efbbe899cbe5e2c688a0b82902632ffcf54c8cee
shubham Shukla <shubham.24269@knit.ac.in>
2026-06-17 12:41:52 +0530

    Custom react Libreary

---
+++ Vite-React/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  
+
+  return (
+    <h1>
+      React with devlopment tool Vite | Shubham Shukla
+    </h1>
+  )
+}
+
+export default App
---
+++ Vite-React/src/shubh.jsx
@@ -0,0 +1,10 @@
+function Shubh(){
+    return(
+        <>
+        <h1> Shubham is ready to learn React</h1>
+        <p> this is a premium course from chai aur code </p>
+        </>
+
+    )
+}
+export default Shubh
