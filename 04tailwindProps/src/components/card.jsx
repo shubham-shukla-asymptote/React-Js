21a5f7fd27e023654859c45846e8661298372b5b
shubham Shukla <shubham.24269@knit.ac.in>
2026-06-19 11:32:50 +0530

    First card of Lionel Messi by self

---
+++ 04tailwindProps/src/components/card.jsx
@@ -0,0 +1,16 @@
+import photo from '../assets/hero.png'
+import REACT from 'react'
+function Card(){
+    return(
+        <div className='  flex justify-center items-center flex-col p-6 h-86 w-80 border-b border-r border-gray-200 m-4 shadow-[0px_8px_12px_-1px_gray] rounded-xl  hover:scale-105 transition'>
+        <img src="https://i1.sndcdn.com/artworks-000360241554-6br2se-t1080x1080.jpg" 
+        alt="profile picture" 
+        className='size-40 rounded-full'/>
+        <h1 className='text-2xl font-bold m-3'>Lionel Messi</h1>
+        <b>Argentina vs Algeria-(3:0)</b>
+        <b>Hattrick By Messi </b>
+        <b>16' 60' 76' </b>
+        </div>
+    )
+}
+export default Card
