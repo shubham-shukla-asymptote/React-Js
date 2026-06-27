0e7a3c51a9fc6d1cb9ee17fba9dcd491ebe93753
shubham Shukla <shubham.24269@knit.ac.in>
2026-06-27 10:59:21 +0530

    context related theme switcher

---
+++ 10themeSwitcher/context/theme.js
@@ -0,0 +1,11 @@
+import React,{createContext} from 'react';
+import { useContext } from 'react';
+export const ThemeContext=createContext({
+    themeMode:"light",
+    darkTheme:()=>{},
+    lightTheme:()=>{}
+})
+export const ThemeProvider=ThemeContext.Provider
+export function useTheme(){
+    return useContext(ThemeContext)
+}
